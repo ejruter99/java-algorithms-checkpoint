41acde078b5c5fb733ebcd7686516932c9eb7dcf
Eli Ruter <ejruter@outlook.com>
2024-07-02 15:05:14 -0500

    Created methods allEqual, lettercount, and interleave and Junit5 tests for each method.

--- src/main/java/com/galvanize/Algorithm.java
+++ src/main/java/com/galvanize/Algorithm.java
@@ -1,4 +1,45 @@
 package com.galvanize;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Algorithm {
+
+    public boolean allEqual(String input) {
+        if (input.isEmpty()) {
+            return false;
+        }
+        String firstLetter = String.valueOf(input.charAt(0)).toLowerCase();
+        for (int x = 0; x < input.length(); x++){
+            if (!String.valueOf(input.charAt(x)).toLowerCase().equals(firstLetter)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Map<String, Long> letterCount(String input) {
+        LinkedHashMap<String, Long> results = new LinkedHashMap<>();
+        for (int x = 0; x < input.length(); x++){
+            String letter = String.valueOf(input.charAt(x)).toLowerCase();
+            if (results.containsKey(letter)){
+                results.put(letter, results.get(letter) + 1L);
+            } else {
+                results.put(letter, 1L);
+            }
+        }
+        return results;
+    }
+
+    public String interleave(List<String> input1, List<String> input2){
+        String result = "";
+        for (int x = 0; x < input1.size(); x++) {
+            result += input1.get(x) + input2.get(x);
+        }
+        return result;
+    }
+
+
 }
--- src/test/java/com/galvanize/AlgorithmTest.java
+++ src/test/java/com/galvanize/AlgorithmTest.java
@@ -1,5 +1,76 @@
 package com.galvanize;
 
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AlgorithmTest {
+
+    Algorithm algorithm;
+    LinkedHashMap<String, Long> results;
+
+    @BeforeEach
+    public void setup(){
+        algorithm = new Algorithm();
+        results = new LinkedHashMap<>();
+    }
+
+    @Test
+    public void testingAAAReturnsTrueForAllEqual(){
+        assertTrue(algorithm.allEqual("aAa"));
+    }
+
+    @Test
+    public void testingABAReturnsFalseForAllEqual(){
+        assertFalse(algorithm.allEqual("Aba"));
+    }
+
+    @Test
+    public void testingEmptyStringReturnsFalseForAllEqual(){
+        assertFalse(algorithm.allEqual(""));
+    }
+
+    @Test
+    public void testingAAEqualsCorrectlyForLetterCount() {
+        results.put("a", 2L);
+        assertEquals(results, algorithm.letterCount("aa"));
+    }
+
+    @Test
+    public void testingABBCDEqualsCorrectlyForLetterCount() {
+        results.put("a", 1L);
+        results.put("b", 2L);
+        results.put("c", 1L);
+        results.put("d", 1L);
+        assertEquals(results, algorithm.letterCount("abBcd"));
+    }
+
+    @Test
+    public void testingBlankEqualsCorrectlyForLetterCount() {
+        assertEquals(results, algorithm.letterCount(""));
+    }
+
+    @Test
+    public void testingBlankEqualsCorrectlyForInterleave() {
+        assertEquals("", algorithm.interleave(Collections.emptyList(), Collections.emptyList()));
+    }
+
+    @Test
+    public void testingTwoArraysInterLeaveCorrectly() {
+        assertEquals("adbecf", algorithm.interleave(Arrays.asList("a", "b", "c"), Arrays.asList("d", "e", "f")));
+    }
+
+    @Test
+    public void testingTwoArraysInterLeaveCorrectlyAgain() {
+        assertEquals("abcdef", algorithm.interleave(Arrays.asList("a", "c", "e"), Arrays.asList("b", "d", "f")));
+    }
+
+
 }
